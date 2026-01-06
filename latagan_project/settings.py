40760b7d4f062853a09dee6de4dcf72e3c0578a7
Latagan Developer <developer@latagan.com>
2026-01-06 18:36:39 +0800

    Fix migrations and use persistent database on Render

--- latagan_project/settings.py
+++ latagan_project/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'latagan_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/data/db.sqlite3' if not DEBUG else BASE_DIR / 'db.sqlite3',
     }
 }
 
